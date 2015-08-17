e413d932c5c7f69224ca2fc57f092bb5fd61fdd3
steven <steven_job@live.com>
2015-08-17 14:18:39 +0100

    add chat window to tabs

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,15 @@
 (function() {
-    var app = angular.module("ChatApp", [ "ngMaterial" ] );
+    var app = angular.module("ChatApp", ["ngMaterial"]).config(function($mdThemingProvider) {
+        $mdThemingProvider.theme("default").primaryPalette("blue").accentPalette("orange");
+    });
 
     app.controller("ChatController", function($scope, $http) {
+
         $scope.loggedIn = false;
+        var tabs = [];
+        var selected = null;
+        var previous = null;
+        $scope.tabs = tabs;
 
         $http.get("/api/user").then(function(userResult) {
             $scope.loggedIn = true;
@@ -15,5 +22,17 @@
                 $scope.loginUri = result.data.uri;
             });
         });
+
+        $scope.addTab = function (title, view) {
+            //todo get view stuff
+            //view = view || title + " Content View";
+            view = "no msgs yet";
+            tabs.push({title: title, content: view, disabled: false});
+        };
+
+        $scope.removeTab = function (tab) {
+            var index = tabs.indexOf(tab);
+            tabs.splice(index, 1);
+        };
     });
 })();
